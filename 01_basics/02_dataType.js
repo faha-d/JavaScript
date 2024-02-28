378e014973538ad6f1a0533c96b5962254cb4566
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-02-28 11:33:21 +0000

    dataTypes in JS

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,31 @@
+"use strict"; // to make our browser treat our code as a newer version of JS.
+
+// alert("hello world") // will not work in Node.JS environment.
+
+/*
+console.log(
+    3 
+    + 
+    3
+) // not good for readability
+
+/*
+DataTypes in JS 
+
+Primitive DataTypes: 
+string => "Fahad"
+number => 2 to the power of 54
+bigInt => if we want to use big numbers
+boolean => true / false
+undefined => 
+null => empty or standalone value
+symbol => unique dataType 
+
+Non- Primitive DataTypes:
+Object =>
+Array =>
+*/
+
+let userName = "Fahad";
+let userAge = 19;
+console.log(typeof userAge) // to check the typeof variable
