9de7aa639e50fb6ec7ba1ed1a833cbd30534cb1a
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-02-29 13:15:04 +0000

    datatypes operations

--- 01_basics/03_dataTypeConversion.js
+++ 01_basics/03_dataTypeConversion.js
@@ -39,3 +39,34 @@ console.log(booleanIsLoggedIn, typeof booleanIsLoggedIn); // Output: "true and b
 const stringLoggedIn = "";
 const convertedStringLoggedIn = Boolean(stringLoggedIn);
 console.log(convertedStringLoggedIn, typeof convertedStringLoggedIn); // Output: "false and boolean
+
+// DataTypes Operations
+
+// Arithmetic operations with numbers
+let value = 3;
+let negValue = -value;
+console.log(negValue); // Output: -3
+
+// String concatenation
+let str1 = "Hello";
+let str2 = " Fahad";
+let str3 = str1 + str2;
+console.log(str3); // Output: "Hello Fahad"
+
+// Concatenating strings and numbers
+console.log("1" + 2); // Output: "12"
+console.log(1 + "2"); // Output: "12"
+console.log("1" + 2 + 2); // Output: "122"
+console.log(1 + 2 + "2"); // Output: "32"
+
+// Complex arithmetic expression
+console.log(((3 + 4) * 5) % 3); // Output: 2 not recommended to do such types of expressions.
+
+// Unary plus operation on boolean and empty string
+console.log(+true); // Output: 1
+console.log(+ ""); // Output: 0
+
+// Incrementing a variable
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); // Output: 101
