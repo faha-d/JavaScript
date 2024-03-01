da3e045c90aceaaad822ce13cfda195f3c72f6e6
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-03-01 20:58:04 +0000

    stack & heap in JS

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,6 +1,6 @@
 // Primitive DataTypes
 
-// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// 7 Types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100;
 const anotherScore = 10.3;
@@ -12,7 +12,7 @@ let userName;
 
 const id = Symbol("123");
 const anotherId = Symbol("123");
-console.log(id === anotherId); // false
+console.log(id === anotherId); // Output: false
 
 const bigNumber = 6565656565656565445n;
 
@@ -20,17 +20,39 @@ const bigNumber = 6565656565656565445n;
 
 // Array, Objects, Functions
 
-const heros = ["ironman", "spiderman", "blackpanther"];
+const heroes = ["ironman", "spiderman", "blackpanther"];
 
 const data = {
   name: "Fahad",
   age: 19,
-  State: "Karachi",
+  state: "Karachi",
 };
 
-const myfunc = () => {
+const myFunc = () => {
   console.log("This is my function");
 };
-myfunc();
+myFunc();
 
-console.log(typeof myfunc);
+console.log(typeof myFunc);
+
+// Memory In JS
+
+// Stack(Primitive Datatypes), Heap(Non-Primitive DataTypes)
+
+let channelName = "codesfahad";
+let anotherName = channelName;
+
+anotherName = "fahadcodes";
+
+console.log(anotherName);
+console.log(channelName);
+
+let userInfo = {
+  name: "Fehdi",
+  email: "fahadcodes@google.com",
+};
+
+let anotherUser = userInfo; // Both objects share the same reference
+anotherUser.email = "codesfahad@google.com";
+console.log(userInfo);
+console.log(anotherUser);
