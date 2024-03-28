c5c59914a1b65c71829b6512da06f0f763592668
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-03-28 11:29:46 +0000

    Numbers and Maths in JS

--- 06_nums_maths.js
+++ 06_nums_maths.js
@@ -8,27 +8,24 @@ console.log(balanceScore.toString()); // Output: '100'
 
 const totalAmount = 1598.36985645;
 console.log(totalAmount.toFixed(2)); // Output: '1598.37'
-// toFixed() is used to format the number with a specific number of digits after the decimal point.
 
 const newAmount = 123.569;
 console.log(newAmount.toPrecision(4)); // Output: '123.6'
-// toPrecision() is used to format the number with a specified total number of significant digits.
 
 const stringNum = 100000;
 console.log(stringNum.toLocaleString("en-PK")); // Output: '100,000'
 
-// ********* MATHS IN JS *********
+console.log(Math.abs(-10)); // Output: 10
+console.log(Math.round(50.69)); // Output: 51
+console.log(Math.ceil(5.1)); // Output: 6
+console.log(Math.floor(5.9)); // Output: 5
+console.log(Math.min(3, 8, 7, 5)); // Output: 3
+console.log(Math.max(3, 8, 7, 5)); // Output: 8
 
-console.log(Math.abs(-10));
-console.log(Math.round(50.69));
-console.log(Math.ceil(5.1));
-console.log(Math.floor(5.9));
-console.log(Math.min(3, 8, 7, 5));
-console.log(Math.max(3, 8, 7, 5));
-
-console.log(Math.random() * 10 + 1);
-console.log(Math.round(Math.random() * 10 + 1));
+console.log(Math.random() * 10 + 1); // Output: Random number between 1 and 10
+console.log(Math.round(Math.random() * 10 + 1)); // Output: Random whole number between 1 and 10
 
 let min = 20;
 let max = 30;
 console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// Output: Random whole number between 20 and 30
