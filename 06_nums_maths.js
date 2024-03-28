d82e953837556e0b22fbff97802271d2587ac0f7
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-03-28 11:18:06 +0000

    Numbers & Maths in JS

--- 06_nums_maths.js
+++ 06_nums_maths.js
@@ -16,3 +16,19 @@ console.log(newAmount.toPrecision(4)); // Output: '123.6'
 
 const stringNum = 100000;
 console.log(stringNum.toLocaleString("en-PK")); // Output: '100,000'
+
+// ********* MATHS IN JS *********
+
+console.log(Math.abs(-10));
+console.log(Math.round(50.69));
+console.log(Math.ceil(5.1));
+console.log(Math.floor(5.9));
+console.log(Math.min(3, 8, 7, 5));
+console.log(Math.max(3, 8, 7, 5));
+
+console.log(Math.random() * 10 + 1);
+console.log(Math.round(Math.random() * 10 + 1));
+
+let min = 20;
+let max = 30;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
