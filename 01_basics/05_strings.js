86cc169e1ed0a736a11aa89d4f03ad1fbe94126a
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-03-28 09:44:45 +0000

    code optimization

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,32 +1,31 @@
-let userName = "Fahad";
-let repoCount = 50;
+const userName = "Fahad";
+const repoCount = 50;
 
 console.log(
   `Hello, my name is ${userName.toUpperCase()} and my Repo Count is ${repoCount}`
 );
 
-let gameName = new String("GTA V");
-let anotherGame = new String("GTA IV");
+const gameName = "GTA V";
+const anotherGame = "GTA IV";
 
-console.log(gameName.length);
-console.log(gameName.concat(anotherGame));
-console.log(gameName.includes("GTA"));
-console.log(anotherGame[0]);
-console.log(anotherGame.startsWith("G"));
-console.log(gameName.charAt(4));
-console.log(gameName.indexOf("A"));
+console.log(gameName.length); // Output: 5
+console.log(gameName + anotherGame); // Output: GTA VGTA IV
+console.log(gameName.includes("GTA")); // Output: true
+console.log(anotherGame[0]); // Output: G
+console.log(anotherGame.startsWith("G")); // Output: true
+console.log(gameName.charAt(4)); // Output: V
+console.log(gameName.indexOf("A")); // Output: 1
 
 const newString = gameName.substring(0, 3);
-const anotherString = anotherGame.slice(-6, 1);
-console.log(newString);
-console.log(anotherString);
+const anotherString = anotherGame.slice(-6, -5);
+console.log(newString); // Output: GTA
+console.log(anotherString); // Output: G
 
 const myName = "     fehdi     ";
-console.log(myName);
-console.log(myName.trim());
+console.log(myName.trim()); // Output: fehdi
 
 const url = "https://www.youtube.com%40";
-console.log(url.replace("%40", "/"));
+console.log(url.replace("%40", "/")); // Output: https://www.youtube.com/
 
 const countryName = "Pakistan";
-console.log(countryName.split(""));
+console.log(countryName.split("")); // Output: [ 'P', 'a', 'k', 'i', 's', 't', 'a', 'n' ]
