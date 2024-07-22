ab05c00f46eb15546434808d18e6a3995bf00d05
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-07-22 20:09:41 +0000

    Object Methods in JS

--- module_02/object.js
+++ module_02/object.js
@@ -36,3 +36,57 @@ myInfo.greetings(); // Call the greetings method
 
 // Freeze the object to prevent further modifications
 Object.freeze(myInfo);
+
+// const snapChatUser = new Object() // Singleton Object
+
+const snapChatUser = {
+  id: "fehdi_here",
+  name: "Fehdi",
+  isLoggedIn: false,
+};
+console.log(snapChatUser);
+
+const regularUser = {
+  email: "abc@gmail.com",
+  fullName: {
+    firstName: "Muhammad",
+    lastName: "Fahad",
+    userName: "fehdi_here",
+  },
+};
+
+console.log(regularUser.fullName); // Will get whole object
+console.log(regularUser.fullName?.firstName); // Will get FirstName inside the fullName object.
+
+const obj1 = { 1: "A", 2: "B", 3: "C" };
+const obj2 = { 4: "D", 5: "E", 6: "F" };
+// const mergedObj = Object.assign({}, obj1, obj2); // empty object is target and remaining objects are source.
+const mergedObj = { ...obj1, ...obj2 };
+console.log(mergedObj);
+
+const users = [
+  {
+    id: 1,
+    email: "fahad@google.com",
+    userName: "Fehdi",
+  },
+  {
+    id: 2,
+    email: "talha@google.com",
+    userName: "Talha",
+  },
+  {
+    id: 3,
+    email: "huzzi@google.com",
+    userName: "Huzzi",
+  },
+];
+
+users.map((users) => {
+  console.log(users);
+});
+
+console.log(Object.keys(snapChatUser));
+console.log(Object.values(snapChatUser));
+console.log(Object.entries(snapChatUser)); // Giving in the array form.
+console.log(snapChatUser.hasOwnProperty('isLoggedIn'))
